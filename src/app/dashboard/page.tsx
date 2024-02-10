eca8c2b096c92a8e4c5fdbdf80607dbb71361a77
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-10 11:20:45 +0900

    try catch をつけてリファクタリング

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,27 +5,35 @@ import { Book } from '@/resource/book'
 
 export default function Page() {
   const [books, setBooks] = useState<Book[]>([])
-  const fetchData = async () => {
-    const apiUrl = 'http://127.0.0.1:8000/bookman/api/books/'
-    const response = await fetch(apiUrl, {
-      method: 'GET',
-    })
-
-    if (response.ok) {
-      const responseData = await response.json()
-      const formattedData: Book[] = responseData.results.map((result: any) => ({
-        id: result.id,
-        name: result.name,
-        leadText: result.lead_text,
-      }))
-      setBooks(formattedData)
-    }
-  }
 
   useEffect(() => {
+    const fetchData = async (): Promise<Book[]> => {
+      const apiUrl = 'http://127.0.0.1:8000/bookman/api/books/'
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+        })
+
+        if (response.ok) {
+          const responseData = await response.json()
+          const formattedData: Book[] = responseData.map((result: any) => ({
+            id: result.id,
+            name: result.name,
+            leadText: result.lead_text,
+          }))
+          setBooks(formattedData)
+          return formattedData
+        } else {
+          console.error('Error fetching data:', response.statusText)
+          return []
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        return []
+      }
+    }
     fetchData()
-      .then((data) => console.log(data))
-      .catch((e) => alert(e))
   }, [])
 
   if (!books) {
