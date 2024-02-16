0ff82d745c3dcade708ad7d4edfed44e4db3870b
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-16 22:50:39 +0900

    topページからダッシュボードに飛ぶようにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          <a href={'/dashboard'}>Lets go to the dashboard</a>
         </p>
         <div>
           <a
