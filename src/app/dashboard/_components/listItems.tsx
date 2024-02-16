39bcc2bd2c9cd45ae7dbc6c54673bce3c7b007a5
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-16 23:22:06 +0900

    ダッシュボードからtopページに飛ぶようにする

--- src/app/dashboard/_components/listItems.tsx
+++ src/app/dashboard/_components/listItems.tsx
@@ -2,7 +2,7 @@ import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import ListSubheader from '@mui/material/ListSubheader'
-import DashboardIcon from '@mui/icons-material/Dashboard'
+import HomeIcon from '@mui/icons-material/Home'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import PeopleIcon from '@mui/icons-material/People'
 import BarChartIcon from '@mui/icons-material/BarChart'
@@ -11,11 +11,11 @@ import AssignmentIcon from '@mui/icons-material/Assignment'
 
 export const mainListItems = (
   <>
-    <ListItemButton>
+    <ListItemButton component='a' href='/'>
       <ListItemIcon>
-        <DashboardIcon />
+        <HomeIcon />
       </ListItemIcon>
-      <ListItemText primary='Dashboard' />
+      <ListItemText primary='Home' />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
