279fc5465ddb5e641377737193ced3be5c809196
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-23 01:50:03 +0900

    qiita ok

--- src/app/dashboard/_components/BookList.tsx
+++ src/app/dashboard/_components/BookList.tsx
@@ -24,7 +24,7 @@ export function BookList({ books }: Props) {
   return (
     <>
       <main>
-        <Box width='100%' marginTop={5}>
+        <Box width='100%'>
           <Typography variant='h4'>本の一覧</Typography>
           <DataGrid columns={columns} rows={rows} />
         </Box>
