3c4e07a517bfe4f6590c688b2ab9b8c20ca947f9
yoshi <yoshitakaOkada0214@gmail.com>
2026-07-22 23:38:56 +0900

    24 simplify reservation condition copy

--- src/app/(bookman)/reservation/_components/PageClient.tsx
+++ src/app/(bookman)/reservation/_components/PageClient.tsx
@@ -156,7 +156,7 @@ export function PageClient({
             予約登録
           </Typography>
           <Alert severity='info'>
-            予約できるのは、同じ支店の同じ本がすべて貸出中または取り置き中で、貸出可能冊数が0冊の支店別所蔵です。同じ本を貸出中の利用者は予約できません。
+            予約できるのは、同じ支店の同じ本に貸出可能冊数が残っていない場合です。同じ本を貸出中の利用者は予約できません。
           </Alert>
           {hasReservableBranchBookStock ? (
             <Alert severity='success'>
